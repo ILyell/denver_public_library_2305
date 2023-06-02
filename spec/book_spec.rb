57b4290b1d45aaf3ea7a729bb9c8023f4908f74b
Ian Lyell <ian.lyell@yahoo.com>
2023-06-02 15:16:37 -0500

    feat: book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
-require 'spec/book_spec'
+require './lib/book'
+require_relative 'book_spec'
 
 RSpec.describe Book do
     before(:each) do
@@ -13,12 +14,13 @@ RSpec.describe Book do
 
         it 'stores 4 arguments from a hash' do
 
-            expect(@book_1[:author_first_name].to eq("Harper")
-            expect(@book_1[:author_last_name].to eq("Lee")
-            expect(@book_1[:title]).to eq("To Kill a Mockingbird")
-            expect(@book_1[:publication_date].to eq("July 11, 1960")
+            expect(@book_1.author_first_name).to eq("Harper")
+            expect(@book_1.author_last_name).to eq("Lee")
+            expect(@book_1.title).to eq("To Kill a Mockingbird")
+            expect(@book_1.publication_date).to eq("July 11, 1960")
             
         end
+    end
         
     describe '#author' do
         it 'returns a string of the authers First and Last name entrys' do
